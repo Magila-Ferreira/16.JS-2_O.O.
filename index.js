30f75683e65531ccaca6d77d36de6266147d8c18
Mágila-Ferreira <magilamorganasf@hotmail.com>
2022-01-13 10:19:15 -0300

    13/01/22 - Reformulação do projeto, após aula 2

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 // Criando a classe Cliente
+// Exportando a classe criada
 class Cliente {
     nome;
     cpf;
+}
+
+// Criando a classe ContaCorrente
+// Exportando a classe criada
+class ContaCorrente {
+    agencia;
+
+    /* #saldo = 0; */
+    _saldo = 0;
+    
+    depositar(valor) {
+        if (valor <= 0) {
+            console.log("\nDepósito inválido! Valor de depósito negativo.");
+            console.log("****************************************************************** ");
+            return valor;
+        }
+
+        this._saldo = this._saldo + valor;
+        console.log("\nDepósito realizado: " + valor);
+        console.log("Saldo atual: " + (this._saldo).toFixed(2));
+
+        carteira = carteira - valor;
+        console.log("CARTEIRA: " + carteira.toFixed(2));
+        console.log("****************************************************************** ");
+    }
+
+    sacar(valor) {
+        if (this._saldo < valor) {
+            console.log("\nSaque não liberado! Você não tem saldo suficientes.");
+            console.log("****************************************************************** ");
+
+            return valor;
+        }
+
+        this._saldo = this._saldo - valor;
+        console.log("\nSaque realizado: " + valor.toFixed(2));
+        console.log("Saldo atual: " + (this._saldo).toFixed(2));
+
+        carteira = carteira + valor;
+        console.log("CARTEIRA: " + carteira.toFixed(2));
+        console.log("****************************************************************** ");
+    }
 }
 
 // Registrando um novo objeto Cliente 
@@ -20,50 +63,6 @@ cliente2.cpf = 88822233309;
 console.log("\n==================================================================================");
 console.log("------------------------------- Byte Bank ----------------------------------------");
 console.log("==================================================================================");
-console.log("\n");
-console.log(cliente1);
-console.log(cliente2);
-
-// Criando a classe ContaCorrente
-class ContaCorrente {
-    agencia;
-    /* #saldo = 0; */
-    _saldo = 0;
-
-
-    depositar(deposito) {
-        if (deposito <= 0) {
-            console.log("\nDepósito inválido! Valor de depósito negativo.");
-            console.log("****************************************************************** ");
-            return deposito;
-        }
-
-        this._saldo = this._saldo + deposito;
-        console.log("\nDepósito realizado: " + deposito.toFixed(2));
-        console.log("Saldo atual: " + (this._saldo).toFixed(2));
-
-        carteira = carteira - deposito;
-        console.log("CARTEIRA: " + carteira.toFixed(2));
-        console.log("****************************************************************** ");
-    }
-
-    sacar(saque) {
-        if (this._saldo < saque) {
-            console.log("\nSaque não liberado! Você não tem saldo suficientes.");
-            console.log("****************************************************************** ");
-
-            return saque;
-        }
-
-        this._saldo = this._saldo - saque;
-        console.log("\nSaque realizado: " + saque.toFixed(2));
-        console.log("Saldo atual: " + (this._saldo).toFixed(2));
-
-        carteira = carteira + saque;
-        console.log("CARTEIRA: " + carteira.toFixed(2));
-        console.log("****************************************************************** ");
-    }
-}
 
 // Registrando um novo objeto ContaCorrente
 const ContaCorrenteRicardo = new ContaCorrente();
@@ -77,7 +76,6 @@ ContaCorrenteRicardo._saldo = 0;
 ContaCorrenteAlice.agencia = 1001;
 ContaCorrenteAlice._saldo = 0;
 
-console.log("\n");
 console.log("********** RICARDO: ");
 console.log(ContaCorrenteRicardo);
 console.log("\n");
@@ -85,8 +83,8 @@ console.log("************ ALICE: ");
 console.log(ContaCorrenteAlice);
 
 // Movimentações da conta_RICARDO:
-console.log("\n**************** Movimentações da conta_RICARDO: *************** ");
-var carteira = 1000;
+console.log("\n**************** Movimentações da conta_RICARDO: ***************** ");
+var carteira = 700;
 var valorDepositado = ContaCorrenteRicardo.depositar(100);
 valorDepositado = ContaCorrenteRicardo.depositar(200);
 valorDepositado = ContaCorrenteRicardo.depositar(-1);
@@ -95,10 +93,10 @@ valorSacado = ContaCorrenteRicardo.sacar(500);
 var carteiraRicardo = carteira;
 
 // Movimentações da conta_ALICE:
-console.log("\n**************** Movimentações da conta_ALICE: *************** ");
-console.log("\n************************ Saldo ALICE: ************************ ");
-carteira = 7000;
-valorDepositado = ContaCorrenteAlice.depositar(5000);
+console.log("\n\n***************** Movimentações da conta_ALICE: ****************** ");
+console.log("\n************************* Saldo ALICE: *************************** ");
+carteira = 6000; 
+valorDepositado  = ContaCorrenteAlice.depositar(5000);
 valorDepositado = ContaCorrenteAlice.depositar(-1500);
 valorDepositado = ContaCorrenteAlice.depositar(300);
 valorSacado = ContaCorrenteAlice.sacar(7000);
@@ -106,10 +104,10 @@ valorSacado = ContaCorrenteAlice.sacar(2500);
 var carteiraAlice = carteira;
 
 // Imprimindo saldo final:
-console.log("\n*********************** Saldo RICARDO: *********************** ");
+console.log("\n----------------------- Saldo RICARDO: --------------------------- ");
 console.log("Saldo Final, ContaCorrente_Ricardo: " + (ContaCorrenteRicardo._saldo).toFixed(2));
-console.log("Saldo Final, CARTEIRA_Ricardo: " + carteiraRicardo.toFixed(2));
+console.log("CARTEIRA: " + carteiraRicardo.toFixed(2));
 
-console.log("\n************************ Saldo ALICE: ************************ ");
+console.log("\n------------------------ Saldo ALICE: ---------------------------- ");
 console.log("Saldo Final, ContaCorrente_Alice: " + (ContaCorrenteAlice._saldo).toFixed(2));
-console.log("Saldo Final, CARTEIRA_Alice: " + carteiraAlice.toFixed(2));
+console.log("CARTEIRA: " + carteiraAlice.toFixed(2));
